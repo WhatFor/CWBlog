106d16dc8617047f4d045c22b501e0d4b464b6b2
Chris Walker <chris@awsm.tech>
2023-01-30 20:25:00 +0000

    Add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: process.env.SITE_URL,
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), tailwind(), sitemap(), partytown()],
   output: "static",
   adapter: vercel()
 });
