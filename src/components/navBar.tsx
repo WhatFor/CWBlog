49f606734b92a88bd4bbf71bf5f75bf4d1641ff3
Chris Walker <chris@awsm.tech>
2023-01-31 23:25:24 +0000

    Add navbar

---
+++ src/components/navBar.tsx
@@ -0,0 +1,66 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import ghIcon from "../../public/images/github-mark.svg";
+
+const NavLinks = [
+    { name: "Home", link: "/" },
+    { name: "Tags", link: "/tags" },
+    { name: "About", link: "/about" },
+]
+
+const NavBar = () => {
+
+    const [showMiniMenu, setShowMiniMenu] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () =>
+            setShowMiniMenu(window.pageYOffset > 100));
+    }, []);
+
+    return (
+        <>
+            <AnimatePresence>
+                {showMiniMenu && (
+
+                    <motion.div
+                        key="mini-nav"
+                        initial={{ y: -100 }}
+                        animate={{ y: 0 }}
+                        exit={{ y: -100 }}
+                        transition={{ duration: 0.3 }}
+                        className="fixed flex mx-auto justify-between inset-x-0 top-2 lg:top-4 z-0 lg:z-10 w-full lg:w-1/3 bg-white rounded-none lg:rounded-xl shadow">
+                        <nav className="w-full grid grid-cols-3 justify-items-stretch">
+                            {NavLinks.map(link => (
+                                <a
+                                    className="text-black text-center px-5 py-2.5 hover:underline"
+                                    href={link.link}>
+                                    {link.name}
+                                </a>
+                            ))}
+                        </nav>
+                    </motion.div>
+
+                )}
+            </AnimatePresence>
+            <div className="flex w-full justify-between bg-white z-20 shadow-sm mb-2">
+                <div></div>
+                <nav className="w-2/3 lg:w-1/3 grid grid-cols-3 justify-items-stretch">
+                    {NavLinks.map(link => (
+                        <a
+                            className="text-black text-center px-5 py-3 hover:underline hover:bg-indigo-100"
+                            href={link.link}>
+                            {link.name}
+                        </a>
+                    ))}
+                </nav>
+                <div className="pr-3 py-3">
+                    <a href="https://github.com/WhatFor">
+                        <img alt="GitHub Logo" src={ghIcon} className="inline-flex w-6 h-6 mr-1 mb-0.5" />
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -1,4 +1,5 @@
 import { motion, useScroll, useSpring } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const ProgressBar = () => {
     const { scrollYProgress } = useScroll();
@@ -8,11 +9,31 @@ const ProgressBar = () => {
         restDelta: 0.001
     });
 
+    const [showBar, setShowBar] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () =>
+            setShowBar(window.pageYOffset > 100));
+    }, []);
+
     return (
-        <motion.div
-            className="bg-indigo-400 h-2 fixed top-0 left-0 right-0 transform-origin-0"
-            style={{ scaleX }}
-            />
+        showBar && (
+            <>
+                <motion.div
+                    key="mini-nav"
+                    initial={{ y: -100 }}
+                    animate={{ y: 0 }}
+                    exit={{ y: -100 }}
+                    transition={{ duration: 0.3 }}
+                    className="bg-slate-200 h-2 fixed top-0 left-0 right-0 d-fixed lg:hidden">
+
+                </motion.div>
+                <motion.div
+                        className="bg-indigo-400 h-2 fixed top-0 left-0 right-0 transform-origin-0"
+                        style={{ scaleX }}
+                    />
+            </>
+        )
     )
 };
 
